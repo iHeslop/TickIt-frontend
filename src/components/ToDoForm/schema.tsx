6bd4008a27ff54ad20c57e22c710a63424556ab7
iHeslop <iheslop@optusnet.com.au>
2024-05-22 15:06:15 +1000

    Added ability for blank content input

--- src/components/ToDoForm/schema.tsx
+++ src/components/ToDoForm/schema.tsx
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 export const schema = z.object({
   title: z.string().min(1),
-  content: z.string().min(1),
+  content: z.string(),
   completed: z.boolean().default(false),
 });
 
